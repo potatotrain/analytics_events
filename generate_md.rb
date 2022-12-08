d87bb004b44ae6b34bc7ef921bdb4c8f5228ea03
muehlemann <mattm@honeycommb.com>
2022-12-08 16:56:35 -0500

    Changes to format

--- generate_md.rb
+++ generate_md.rb
@@ -12,7 +12,7 @@ File.open('mixpanel_events.md', 'w+') do |file|
   # Iterates over JSON and writes events
   json.values.each do |event|
     title = "\#\#\# #{event['key'].upcase.gsub!('_', ' ')}"
-    key   = "```#{event['key']}```"
+    key   = "```\n#{event['key']}\n```"
     desc  = event['description']
 
     file.write("#{title}\n#{key}\n\n#{desc}\n---\n\n")
